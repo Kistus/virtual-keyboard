7d1f93b08d233b50a7a0af8faa7dc1fef2615df1
Kanstantsin Kalenik <kostakalenik2002.09k@gmail.com>
2023-05-01 19:07:43 +0200

    fix:caps and shift

--- index.js
+++ index.js
@@ -1,34 +1,72 @@
+let CapsLockIsOn = false;
+let ShiftIsOn = false;
+
+
 const keys = [
     ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
     ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
     ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
-    ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift'],
-    ['Ctrl', 'Alt', 'Space', 'Alt', 'Ctrl']
+    ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/','/|\\', 'Shift'],
+    ['Ctrl', 'Alt', 'Space', 'Alt', 'Ctrl','<-','\\|/', '->']
+  ];
+
+  const keysShift = [
+    ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'],
+    ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
+    ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
+    ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/','/|\\', 'Shift'],
+    ['Ctrl', 'Alt', 'Space', 'Alt', 'Ctrl','<-','\\|/', '->']
   ];
   
   const textField = document.createElement('textarea');
   document.body.appendChild(textField);
   
+  function keyboard(CapsLock,Shift){
+
   // Create the HTML for the keyboard
   const keyboardContainer = document.createElement('div');
   keyboardContainer.classList.add('keyboard-container');
-  document.body.appendChild(keyboardContainer);
+  if(document.querySelector('.keyboard-container'))
+  document.body.removeChild(document.querySelector('.keyboard-container'));
   
-  for (let i = 0; i < keys.length; i++) {
+  mykeys = Shift ? keysShift : keys;
+  
+  for (let i = 0; i < mykeys.length; i++) {
     const row = document.createElement('div');
     row.classList.add('keyboard-row');
     keyboardContainer.appendChild(row);
   
-    for (let j = 0; j < keys[i].length; j++) {
+    for (let j = 0; j < mykeys[i].length; j++) {
       const key = document.createElement('div');
       key.classList.add('keyboard-key');
-      key.textContent = keys[i][j];
+      if(CapsLock || Shift)
+        key.textContent = mykeys[i][j].toUpperCase();
+    else
+        key.textContent = mykeys[i][j];
       row.appendChild(key);
   
       // Add event listener for when the key is clicked
       key.addEventListener('mousedown', () => {
         key.classList.add('active');
-        simulateKeyPress(key.textContent);
+        if (key.textContent.toLowerCase() === 'capslock'){
+            CapsLockIsOn = !CapsLockIsOn;
+            keyboard(CapsLockIsOn,ShiftIsOn);
+        }
+        else if(key.textContent.toLowerCase() === 'shift'){
+            ShiftIsOn = !ShiftIsOn;
+            keyboard(CapsLockIsOn,ShiftIsOn);
+        }
+        else{
+            if(CapsLockIsOn)
+                simulateKeyPress(key.textContent.toUpperCase());
+            else if(ShiftIsOn){
+                simulateKeyPress(key.textContent.toUpperCase());
+                ShiftIsOn = false;
+                keyboard(CapsLockIsOn,ShiftIsOn);
+            }
+            else
+                simulateKeyPress(key.textContent);
+        }
       });
 
         key.addEventListener('mouseup', () => {
@@ -52,6 +90,11 @@ const keys = [
       });
     }
   }
+  document.body.appendChild(keyboardContainer);
+
+}
+
+keyboard(CapsLockIsOn,ShiftIsOn);
   
   // Simulate a key press on the virtual keyboard
   function simulateKeyPress(key) {
